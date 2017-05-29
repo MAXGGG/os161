442e2e14487e457fb77846dcad07546746a3e9be
Max Yin <maxyin@Maxs-MacBook-Pro.local>
2017-05-28 22:12:56 -0400

    dasda

--- kern/synchprobs/traffic_synch.c
+++ kern/synchprobs/traffic_synch.c
@@ -21,9 +21,65 @@
 /*
  * replace this with declarations of any synchronization and other variables you need here
  */
-static struct semaphore *intersectionSem;
+static struct lock *lock;
+static struct cv *cv;
 
+typedef struct Vehicles
+{
+  Direction origin;
+  Direction destination;
+} Vehicle;
 
+typedef struct Intersections
+{
+  Vehicle volatile cars[10];
+  int current_number = 0;
+} Intersetion;
+
+bool check_can_enter(Intersection, Vehicle);
+bool if_collide(Vehicle, Vehicle);
+
+bool right_turn(Vehicle);
+void add_car_to_intersection(Intersection, Vehicle);
+void remove_car_from_intersection(Intersection, Vehicle);
+
+bool
+right_turn(Vehicle v) {
+  KASSERT(v != NULL);
+  if (((v.origin == west) && (v.destination == south)) ||
+      ((v.origin == south) && (v.destination == east)) ||
+      ((v.origin == east) && (v.destination == north)) ||
+      ((v.origin == north) && (v.destination == west))) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool
+check_can_enter(Intersection i, Vheicle v){
+  for(int i=0;i<10;++i){
+    if(i.cars[i]!=NULL){
+      if(!if_collide(i.cars[i], v))
+        return false;
+    }
+  }
+  return true;
+}
+
+bool
+if_collide(Vehicle v1, Vehecle v2){
+  if(v1.origin==v2.origin){
+    return true;
+  }else if(v1.origin==v2.destination&&v1.destination==v2.origin){
+    return true;
+  }else if(v1.destination!=v2.destination&&(right_turn(v1)||right_turn(v2))){
+    return true;
+  }
+  return false;
+}
+
+Intersection volatile i;
 /* 
  * The simulation driver will call this function once before starting
  * the simulation
@@ -36,13 +92,37 @@ intersection_sync_init(void)
 {
   /* replace this default implementation with your own implementation */
 
-  intersectionSem = sem_create("intersectionSem",1);
-  if (intersectionSem == NULL) {
-    panic("could not create intersection semaphore");
+  lock = lock_create("lock");
+  cv = cv_create("cv");
+  if (lock == NULL) {
+    panic("could not create lock");
   }
+  if (cv == NULL) {
+    panic("could not create cv");
+  }
+
+  for(int i=0; i<10;++i){
+    i.cars[i] = NULL;
+  }
+
   return;
 }
 
+void 
+add_car_to_intersection(Intersection i, Vehicle v){
+  i.[current_number] = v;
+  ++current_number;
+}
+
+void
+remove_car_from_intersection(Intersection i, Direction o, Direction d){
+  for(int i=0;i<10;++i){
+    if(i.cars[i]!=NULL&&i.cars[i].origin==o&&i.cars[i].destination==d){
+      i.cars[i] = NULL;
+      break;
+    }
+  }
+}
 /* 
  * The simulation driver will call this function once after
  * the simulation has finished
@@ -54,8 +134,10 @@ void
 intersection_sync_cleanup(void)
 {
   /* replace this default implementation with your own implementation */
-  KASSERT(intersectionSem != NULL);
-  sem_destroy(intersectionSem);
+  KASSERT(lock != NULL);
+  KASSERT(cv != NULL);
+  lock_destroy(lock);
+  cv_destroy(cv);
 }
 
 
@@ -76,10 +158,17 @@ void
 intersection_before_entry(Direction origin, Direction destination) 
 {
   /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  P(intersectionSem);
+  KASSERT(cv != NULL);
+  KASSERT(lock != NULL);
+  Vehicle v;
+  v.origin = origin;
+  v.destination = destination;
+  lock_acquire(lock);
+  while(check_can_enter(i,v)){
+    cv_wait(cv, lock);
+  }
+  add_car_to_intersection(i, v);
+  lock_release(lock);
 }
 
 
@@ -98,8 +187,10 @@ void
 intersection_after_exit(Direction origin, Direction destination) 
 {
   /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  V(intersectionSem);
+  KASSERT(cv != NULL);
+  KASSERT(lock != NULL);
+  lock_acquire(lock);
+  remove_car_from_intersection(i, origin, destination);
+  cv_broadcast(cv, lock);
+  lock_release(lock);
 }
