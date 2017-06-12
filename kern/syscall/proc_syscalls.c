112e0ecf4f51313db5592e468e4936d61d3bd547
Max Yin <maxyin@Maxs-MacBook-Pro.local>
2017-06-11 23:30:11 -0400

    ds

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -129,6 +129,7 @@ sys_waitpid(pid_t pid,
      DEBUG(DB_EXEC,"pid: %d is being blocked \n",(int)parent->p_id);
      cv_wait(parent->p_cv, parent->p_cv_lock);
   }
+  DEBUG(DB_EXEC,"pid: %d is released \n",(int)parent->p_id);
   lock_release(parent->p_cv_lock);
   exitstatus = child->p_exitcode;
   #else
