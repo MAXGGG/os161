cfbab8fa0ca8da6d778e44e8a7b5c5597e1e345c
Max Yin <maxyin@Maxs-MacBook-Pro.local>
2017-07-02 20:50:26 -0400

    a2b

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -7,6 +7,7 @@
 #include <current.h>
 #include <proc.h>
 #include <thread.h>
+#include <limits.h>
 #include <addrspace.h>
 #include <copyinout.h>
 #include <mips/trapframe.h>
@@ -196,4 +197,65 @@ sys_fork(struct trapframe *tf, pid_t *retval)
 
    return 0;
 }
+
+int
+sys_execv(const char *program, char **args){
+   struct addrspace *as;
+   struct vnode *v;
+   vaddr_t entrypoint, stackptr;
+   int result;
+
+   char* program_path = kmalloc(sizeof(char)*strlen(program));
+   if(!program_path){
+         return ENOMEM;
+   }
+   strcpy(program_path, program);
+
+   /* Open the file. */
+   result = vfs_open(program_path, O_RDONLY, 0, &v);
+   if (result) {
+      return result;
+   }
+
+   /* We should be a new process. */
+   KASSERT(curproc_getas() == NULL);
+
+   /* Create a new address space. */
+   as = as_create();
+   if (as ==NULL) {
+      vfs_close(v);
+      return ENOMEM;
+   }
+
+   /* Switch to it and activate it. */
+   curproc_setas(as);
+   as_activate();
+
+   /* Load the executable. */
+   result = load_elf(v, &entrypoint);
+   if (result) {
+      /* p_addrspace will go away when curproc is destroyed */
+      vfs_close(v);
+      return result;
+   }
+
+   /* Done with the file now. */
+   vfs_close(v);
+
+   /* Define the user stack in the address space */
+   result = as_define_stack(as, &stackptr);
+   if (result) {
+      /* p_addrspace will go away when curproc is destroyed */
+      return result;
+   }
+
+   /* Warp to user mode. */
+   enter_new_process(0 /*argc*/, NULL /*userspace addr of argv*/,
+           stackptr, entrypoint);
+
+   /* enter_new_process does not return. */
+   panic("enter_new_process returned\n");
+   return EINVAL;
+}
+
 #endif
