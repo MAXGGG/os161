c2585ea1df6e795e132d0f525f7cfba2f3a4382c
Max Yin <maxyin@Maxs-MacBook-Pro.local>
2017-07-03 22:05:46 -0400

    sda

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -244,6 +244,7 @@ sys_execv(userptr_t program, userptr_t args){
    for(int i=0;i<argc;++i){
       size_t length = strlen(pname)+1;
       DEBUG(DB_EXEC, "legnth is %d \n", length );
+      DEBUG(DB_EXEC, "string is %s \n", arg_a[i] );
       argv[i] = kmalloc(length);
       DEBUG(DB_EXEC, "km is done hoyeeeeeee");
       if(argv[i]){
