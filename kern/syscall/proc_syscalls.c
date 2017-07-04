af5d1b872c69e1082b2ef49d21986d3e7c89b601
Max Yin <maxyin@Maxs-MacBook-Pro.local>
2017-07-03 21:11:44 -0400

    a2d

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -242,8 +242,10 @@ sys_execv(userptr_t program, userptr_t args){
       int length = strlen(arg_a[i])+1;
       DEBUG(DB_EXEC, "legnth is %d \n", length );
       argv[i] = kmalloc(strlen(arg_a[i])+1);
+      DEBUG(DB_EXEC, "km is done hoyeeeeeee");
       if(argv[i]){
          result = copyinstr((userptr_t)arg_a[i], argv[i], strlen(arg_a[i])+1, NULL);
+         DEBUG(DB_EXEC, "copy in is done as wellllll");
          if(result){
             return result;
          }
