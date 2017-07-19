f9a776f4fb149452e7021279a23d8e3db7dd75f1
Max Yin <maxyin@Maxs-MacBook-Pro.local>
2017-07-19 02:04:19 -0400

    a3

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -39,6 +39,8 @@
 #include <vm.h>
 #include <mainbus.h>
 #include <syscall.h>
+#include "opt-A2.h"
+#include "opt-A3.h"
 
 
 /* in exception.S */
@@ -111,10 +113,65 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 	/*
 	 * You will probably want to change this.
 	 */
+	 #if OPT_A3
+	 struct addrspace *as;
+	 struct proc *p = curproc;
+	 /* for now, just include this to keep the compiler from complaining about
+		 an unused variable */
+	 #if OPT_A2
+	 KASSERT(p!=NULL);
+	 //set current process state to exit
+	 p->p_state = 1;
+	 p->p_exitcode = _MKWAIT_EXIT(exitcode);
+	 if(p->p_parent!=NULL){
+
+	   //save exit information into parent status array
+	   struct childrenStatus* cs = getChildrenByPid(p->p_parent, p->p_id);
+	   cs->p_exitcode = _MKWAIT_SIG(exitcode);
+	   cs->p_state = 1;
+
+	   lock_acquire(p->p_parent->p_cv_lock);
+	   //wake up parent
+	   cv_broadcast(p->p_parent->p_cv, p->p_parent->p_cv_lock);
+	   lock_release(p->p_parent->p_cv_lock);
+	 }
+	 #else
+	 (void)exitcode;
+	 #endif
+
+	 KASSERT(curproc->p_addrspace != NULL);
+	 as_deactivate();
+
+	 /*
+	  * clear p_addrspace before calling as_destroy. Otherwise if
+	  * as_destroy sleeps (which is quite possible) when we
+	  * come back we'll be calling as_activate on a
+	  * half-destroyed address space. This tends to be
+	  * messily fatal.
+	  */
+	  // DEBUG(DB_EXEC, "sys exiting 3\n");
+	 as = curproc_setas(NULL);
+	 as_destroy(as);
+
+	 /* detach this thread from its process */
+	 /* note: curproc cannot be used after this call */
+	 proc_remthread(curthread);
+	 // DEBUG(DB_EXEC, "sys exiting 4\n");
+	 /* if this is the last user process in the system, proc_destroy()
+		 will wake up the kernel menu thread */
+	 proc_destroy(p);
+	 // DEBUG(DB_EXEC, "sys exiting 5\n");
+
+	 thread_exit();
+	 // DEBUG(DB_EXEC, "sys exiting finished\n");
+	 /* thread_exit() does not return, so we should never get here */
+	 panic("return from thread_exit in sys_exit\n");
+	 #else
 
 	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
 		code, sig, trapcodenames[code], epc, vaddr);
 	panic("I don't know how to handle this\n");
+	#endif
 }
 
 /*
@@ -217,7 +274,7 @@ mips_trap(struct trapframe *tf)
 		KASSERT(curthread->t_curspl == 0);
 		KASSERT(curthread->t_iplhigh_count == 0);
 
-		DEBUG(DB_SYSCALL, "syscall: #%d, args %x %x %x %x\n", 
+		DEBUG(DB_SYSCALL, "syscall: #%d, args %x %x %x %x\n",
 		      tf->tf_v0, tf->tf_a0, tf->tf_a1, tf->tf_a2, tf->tf_a3);
 
 		syscall(tf);
@@ -248,11 +305,11 @@ mips_trap(struct trapframe *tf)
 	case EX_IBE:
 	case EX_DBE:
 		/*
-		 * This means you loaded invalid TLB entries, or 
-		 * touched invalid parts of the direct-mapped 
+		 * This means you loaded invalid TLB entries, or
+		 * touched invalid parts of the direct-mapped
 		 * segments. These are serious kernel errors, so
 		 * panic.
-		 * 
+		 *
 		 * The MIPS won't even tell you what invalid address
 		 * caused the bus error.
 		 */
@@ -282,8 +339,8 @@ mips_trap(struct trapframe *tf)
 	 * set by copyin/copyout and related functions to signify that
 	 * the addresses they're accessing are userlevel-supplied and
 	 * not trustable. What we actually want to do is resume
-	 * execution at the function pointed to by badfaultfunc. That's 
-	 * going to be "copyfail" (see copyinout.c), which longjmps 
+	 * execution at the function pointed to by badfaultfunc. That's
+	 * going to be "copyfail" (see copyinout.c), which longjmps
 	 * back to copyin/copyout or wherever and returns EFAULT.
 	 *
 	 * Note that we do not just *call* this function, because that
@@ -308,7 +365,7 @@ mips_trap(struct trapframe *tf)
 
 	kprintf("panic: Fatal exception %u (%s) in kernel mode\n", code,
 		trapcodenames[code]);
-	kprintf("panic: EPC 0x%x, exception vaddr 0x%x\n", 
+	kprintf("panic: EPC 0x%x, exception vaddr 0x%x\n",
 		tf->tf_epc, tf->tf_vaddr);
 
 	panic("I can't handle this... I think I'll just die now...\n");
